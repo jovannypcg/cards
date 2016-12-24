e888c4af0d581585429bb297d2b3cd7446d53431
Jovanny Cruz <jovannypcg@yahoo.com>
2016-12-24 14:52:17 -0600

    Add test cases for Cards module

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,7 +1,26 @@
 defmodule CardsTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck size should be 20" do
+    deck = Cards.create_deck
+
+    assert length(deck) == 20
+  end
+
+  test "shuffle should mix up a deck" do
+    deck = Cards.create_deck
+
+    assert deck != Cards.shuffle(deck)
+  end
+
+  test "save should create a file containing a deck" do
+    filename = "mydeck"
+
+    {hand, _deck_remainder} = Cards.create_hand(3)
+
+    assert :ok == Cards.save(hand, filename)
+
+    # Cleaning up the file
+    File.rm!(filename)
   end
 end
